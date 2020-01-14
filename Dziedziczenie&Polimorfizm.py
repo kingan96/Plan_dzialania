101f9b482e4ee2432beb2e500b4f78bedd49d6be
kingan96 <56343345+kingan96@users.noreply.github.com>
2020-01-14 20:34:50 +0100

    Create Dziedziczenie&Polimorfizm.py

---
+++ Dziedziczenie&Polimorfizm.py
@@ -0,0 +1,37 @@
+# Dziedziczenie
+
+class Rectangle (object):
+        def __init__(self,w,h):
+            self.w = w
+            self.h = h
+            def area(self):
+                return self.w * self.h
+            
+class Square (Rectangle):
+     def __init__(self,a):
+
+         super()__init(a,a)__
+   
+r = Rectangle(3,7)       
+s = Square(4)  
+print(s.area())    
+           
+
+# Polimorfizm
+class Rectangle (object):
+        def __init__(self,w,h):
+            self.w = w
+            self.h = h
+            def area(self):
+                return self.w * self.h
+            def angle(self):
+                
+ class Square (Rectangle):
+     def __init__(self,a):
+
+         super()__init(a,a)__
+     def angle(self):
+         return 90
+       
+s = Square(4)  
+print(s.angle())   
