3a15d5aebbe9fb7c02791f5a3000863c7cf49e7c
kingan96 <56343345+kingan96@users.noreply.github.com>
2020-01-15 12:30:27 +0100

    Create zolw.py

---
+++ zolw.py
@@ -0,0 +1,13 @@
+# Python program to draw square  
+# using Turtle Programming 
+import turtle  
+dave = turtle.Turtle() 
+dave.color('red')
+dave.pensize(2)
+dave.shape('turtle')
+  
+for i in range(4): 
+    dave.forward(50) 
+    dave.right(90) 
+      
+turtle.done() 
